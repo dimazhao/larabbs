848b8fb568bb7a24f10c120c65325aee1cb64f14
dima <625570555@qq.com>
2025-09-28 12:55:00 +0000

    用户个人页面原型

---
+++ resources/views/users/show.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.app')
+@section('title', $user->name.'个人中心')
+@section('content')
+
+    <div class="row">
+       <div class="col-lg-3 col-md-3 hidden-sm hidden-xs user-info">
+           <div class="card">
+                <img class="card-img-top"
+                src="https://img2.woyaogexing.com/2025/04/21/b3d8a772ff7b255e3cb3cda9edbbe3d0.jpg"
+                alt="{{ $user->name }}">
+                <div class="card-body">
+                    <h5><strong>个人简介</strong></h5>
+                    <p>暂无简介</p>
+                    <hr>
+                    <h5><strong>注册于</strong></h5>
+                    <p>{{ $user->created_at->diffForHumans() }}</p>
+
+                </div>
+           </div>
+       </div>
+       <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12">
+           <div class="card">
+               <div class="card-body">
+                <h1 class="mb-0" style="font-size:22px;">{{ $user->name }} <small>{{ $user->email}}</small></h1>
+               </div>
+            </div>
+            <hr>
+
+            <div class="card">
+                <div class="card-body">
+                    这里是用户个人中心
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+@stop
--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,5 @@ Auth::routes();
 Auth::routes(['verify' => true]);
 
 Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
+
+Route::resource('users','UsersController',['only'=>['show','update','edit']]);
