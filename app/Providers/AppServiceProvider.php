2c7ae0842dd211a38e4df3e3f4aea91b21682fcf
dima <625570555@qq.com>
2025-10-01 05:13:46 +0000

    集成 administrator

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -30,6 +30,10 @@ class AppServiceProvider extends ServiceProvider
 		\App\Models\Reply::observe(\App\Observers\ReplyObserver::class);
 		\App\Models\Topic::observe(\App\Observers\TopicObserver::class);
         \Illuminate\Pagination\Paginator::useBootstrap();
+        // 加载自定义配置文件
+    $userConfig = require config_path('administrator/users.php');
+    // 合并到框架配置中（键为 administrator.users）
+    config()->set('administrator.users', $userConfig);
         //
     }
 }
